3f4656a66257b356afa00d5c153f87655ca85727
Guilherme Felipe <glipecoelho2@gmail.com>
2024-11-11 03:16:27 -0300

    Initial commit

---
+++ src/test/java/com/project/pdv/pdv/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pdv.pdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
